b435bb8d7d0ae24b8909b4cb98582f48e9246902
Cjay Landero <xjaylandero@gmail.com>
2024-03-01 18:04:21 +0800

    Add position later

--- src/app/admin/manage-faculty/manage-faculty.component.ts
+++ src/app/admin/manage-faculty/manage-faculty.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { GcBoxComponent } from './gc-box/gc-box.component';
+import { PersonalInfoFormComponent } from './personal-info-form/personal-info-form.component';
 @Component({
   selector: 'app-manage-faculty',
   standalone: true,
-  imports: [GcBoxComponent],
+  imports: [GcBoxComponent, PersonalInfoFormComponent],
   templateUrl: './manage-faculty.component.html',
   styleUrl: './manage-faculty.component.css'
 })
