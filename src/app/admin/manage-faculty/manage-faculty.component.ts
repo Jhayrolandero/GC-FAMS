83903f7a9d8abc5843b8d31c2879e0d3afafd64a
Cjay Landero <xjaylandero@gmail.com>
2024-03-03 10:24:33 +0800

    added dynamic layout to college input

--- src/app/admin/manage-faculty/manage-faculty.component.ts
+++ src/app/admin/manage-faculty/manage-faculty.component.ts
@@ -3,16 +3,22 @@ import { GcBoxComponent } from './gc-box/gc-box.component';
 import { PersonalInfoFormComponent } from './personal-info-form/personal-info-form.component';
 import { CollegeService } from '../../services/admin/college.service';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
 
 export interface college {
+  map(arg0: (item: any) => any): any;
   'college_id': number;
   'college_name': string;
+  'college_abbv': string;
+  'bgColor': string;
+  'imgPath': string;
+
 }
 
 @Component({
   selector: 'app-manage-faculty',
   standalone: true,
-  imports: [GcBoxComponent, PersonalInfoFormComponent, ReactiveFormsModule],
+  imports: [GcBoxComponent, PersonalInfoFormComponent, ReactiveFormsModule, NgFor],
   providers: [CollegeService],
   templateUrl: './manage-faculty.component.html',
   styleUrl: './manage-faculty.component.css'
@@ -37,8 +43,9 @@ facultyInfo = new FormGroup({
     language: new FormControl(''),
     nationality: new FormControl(''),
     college: new FormControl(''),
-    department: new FormControl(''),
-    position: new FormControl('')
+    course: new FormControl(''),
+    position: new FormControl(''),
+    employment: new FormControl(''),
   });
 
   setCollege(value: string): void {
@@ -47,11 +54,24 @@ facultyInfo = new FormGroup({
     });
   }
 
-  setCourse(value: string): void {
+  setPosition(value: string): void {
     this.facultyInfo.patchValue({
       college: value
     });
   }
+
+  setCourse(value: string): void {
+    this.facultyInfo.patchValue({
+      course: value
+    })
+  }
+
+  setEmployment(value: string): void {
+    this.facultyInfo.patchValue({
+      employment: value
+    })
+  }
+
   onSubmit() {
     // TODO: Use EventEmitter with form value
     console.log(this.facultyInfo.value);
@@ -65,7 +85,69 @@ facultyInfo = new FormGroup({
 
   fetchCollege():void {
     this.College.fetchCollege().subscribe(
-      colleges => this.colleges = colleges
+      colleges => {
+        this.colleges = this.modifyData(colleges)
+
+        console.log(this.colleges)
+      }
     )
   }
+
+
+
+  abbvCollege(college: string): string {
+    const abbv = college.match(/[A-Z]/g) || [];
+    return abbv.join("");
+  }
+
+  modifyData(data: college): any {
+    // Example: Add a new property to each item
+    return data.map((item: college) => {
+
+      switch(item.college_name.toLowerCase()) {
+        case 'college of computer studies':
+          return {
+            ...item,
+            college_abbv: this.abbvCollege(item.college_name),
+            bgColor: '#FF7A00',
+            imgPath: '../../../../assets/college-logo/ccs.png'
+            };
+          case 'college of business and accountancy':
+            return {
+              ...item,
+              college_abbv: this.abbvCollege(item.college_name),
+              bgColor: '#FFDF00',
+              imgPath: '../../../../assets/college-logo/cba.png'
+            };
+          case 'college of education, arts, and sciences':
+            return {
+              ...item,
+              college_abbv: this.abbvCollege(item.college_name),
+              bgColor: '#0077CC',
+              imgPath: '../../../../assets/college-logo/ceas.png'
+            };
+          case 'college of allied health studies':
+            return {
+              ...item,
+              college_abbv: this.abbvCollege(item.college_name),
+              bgColor: '#FF0000',
+              imgPath: '../../../../assets/college-logo/cahs.png'
+            };
+          case 'college of hospitality and tourism management':
+            return {
+              ...item,
+              college_abbv: this.abbvCollege(item.college_name),
+              bgColor: '#FF0082',
+              imgPath: '../../../../assets/college-logo/chtm.png'
+            };
+        default:
+          return {
+            ...item,
+            newProperty: 'default'
+          };
+      }
+    });
+  }
+
+
 }
