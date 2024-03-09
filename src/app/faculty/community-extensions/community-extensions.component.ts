d2e3dd34452fa7f4dacb5a5da848342a01a27658
Cjay Landero <xjaylandero@gmail.com>
2024-03-09 12:27:40 +0800

    added spinner to async components

--- src/app/faculty/community-extensions/community-extensions.component.ts
+++ src/app/faculty/community-extensions/community-extensions.component.ts
@@ -4,15 +4,16 @@ import { OtherCommexComponent } from './other-commex/other-commex.component';
 import { CommonModule, NgFor, SlicePipe } from '@angular/common';
 import { FacultyFetcherService } from '../../services/faculty/faculty-fetcher.service';
 import { mainPort } from '../../app.component';
-
+import { LoadingScreenComponent } from '../../components/loading-screen/loading-screen.component';
 @Component({
   selector: 'app-community-extensions',
   standalone: true,
-  imports: [OtherCommexComponent, NgFor, SlicePipe, CommonModule],
+  imports: [OtherCommexComponent, NgFor, SlicePipe, CommonModule, LoadingScreenComponent],
   templateUrl: './community-extensions.component.html',
   styleUrl: './community-extensions.component.css'
 })
 export class CommunityExtensionsComponent{
+  isLoading: boolean = true;
   commexs: CommunityExtension[] = [];
   min: number = 100;
   max: number = 250;
@@ -21,13 +22,24 @@ export class CommunityExtensionsComponent{
     this.getCommex();
   }
 
+  // getCommex():void {
+  //   this.facultyService.fetchCommex().subscribe((next) => {
+  //     this.commexs = next;
+  //     this.dateSorter();
+  //     this.commexs.forEach(this.parseImageLink);
+  //   }, (error) => {
+  //     console.log(error);
+  //   });
+  // }
   getCommex():void {
-    this.facultyService.fetchCommex().subscribe((next) => {
-      this.commexs = next;
-      this.dateSorter();
-      this.commexs.forEach(this.parseImageLink);
-    }, (error) => {
-      console.log(error);
+    this.facultyService.fetchCommex().subscribe({
+      next: (next) =>  this.commexs = next,
+      error: (error) => console.log(error),
+      complete: () => {
+        this.dateSorter();
+        this.commexs.forEach(this.parseImageLink);
+        this.isLoading = false
+      }
     });
   }
 
