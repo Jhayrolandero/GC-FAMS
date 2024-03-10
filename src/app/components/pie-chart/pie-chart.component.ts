f6bc574889bb173f64713cdcc02dceb3319778f1
Cjay Landero <xjaylandero@gmail.com>
2024-03-10 12:48:23 +0800

    added layout to analytics

---
+++ src/app/components/pie-chart/pie-chart.component.ts
@@ -0,0 +1,48 @@
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxChartsModule, ScaleType } from '@swimlane/ngx-charts';
+import { single } from '../../faculty/analytics/data';
+@Component({
+  selector: 'app-pie-chart',
+  standalone: true,
+  imports: [NgxChartsModule],
+  templateUrl: './pie-chart.component.html',
+  styleUrl: './pie-chart.component.css',
+  encapsulation: ViewEncapsulation.None
+})
+export class PieChartComponent implements OnInit {
+  @Input('data') data: any[] = []
+  single: any[] | undefined;
+  view: any[] = [700, 400];
+
+  // options
+  gradient: boolean = false;
+  showLegend: boolean = false;
+  showLabels: boolean = true;
+  isDoughnut: boolean = false;
+  explodeSlices: boolean = false;
+
+  colorScheme = {name: 'myScheme',
+  selectable: true,
+  group: ScaleType.Ordinal,
+    domain: ['#fbd650', '#65c280', '#1f7cb5', '#ef6540']
+  };
+
+  constructor() {
+  }
+  ngOnInit(): void {
+    this.single = this.data
+  }
+
+  onSelect(data: any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data: any): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data: any): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
+}
--- src/app/faculty/analytics/analytics.component.ts
+++ src/app/faculty/analytics/analytics.component.ts
@@ -1,19 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FacultyMilestoneCalendarComponent } from '../../components/faculty/faculty-milestone-calendar/faculty-milestone-calendar.component';
 import { FacultyMilestoneHistoryComponent } from '../../components/faculty/faculty-milestone-history/faculty-milestone-history.component';
+import { PieChartComponent } from '../../components/pie-chart/pie-chart.component';
+import { single } from './data';
+import { Init } from 'v8';
 
 @Component({
   selector: 'app-analytics',
   standalone: true,
   imports: [
-    FacultyMilestoneCalendarComponent, 
-    FacultyMilestoneHistoryComponent
+    FacultyMilestoneCalendarComponent,
+    FacultyMilestoneHistoryComponent,
+    PieChartComponent,
   ],
   templateUrl: './analytics.component.html',
   styleUrl: './analytics.component.css'
 })
-export class AnalyticsComponent {
+export class AnalyticsComponent implements OnInit{
+
+  single: any[] = []
   constructor(){
     console.log("Test render");
+  }
+
+  ngOnInit(): void {
+    this.single = [...single]
+    console.log(this.single)
   }
 }
