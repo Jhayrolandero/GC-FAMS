5a2c2c8fcb08b5a4e570a949d3bbbaa12b4d186f
Cjay Landero <xjaylandero@gmail.com>
2024-03-11 07:29:46 +0800

    merge with main

--- src/app/components/cv/cv.component.ts
+++ src/app/components/cv/cv.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component , ViewChild, ElementRef, AfterViewInit, OnInit} from '@angular/core';
 import { ModalComponent } from '../modal/modal.component';
+import { jsPDF } from "jspdf";
 @Component({
   selector: 'app-cv',
   standalone: true,
@@ -7,6 +8,27 @@ import { ModalComponent } from '../modal/modal.component';
   templateUrl: './cv.component.html',
   styleUrl: './cv.component.css'
 })
-export class CvComponent {
-  
+export class CvComponent implements OnInit {
+  doc: any = new jsPDF('p', 'in', [612, 792])
+
+  @ViewChild('cvElement') cvElement!: ElementRef;
+
+  ngAfterViewInit() {
+    console.log(this.cvElement.nativeElement);
+    this.saveToPDF()
+
+  }
+  // Source HTMLElement or a string containing HTML.
+  elementHTML = document.getElementById('cv');
+  saveToPDF() {
+    this.doc.html(this.cvElement.nativeElement, {
+      callback: function (doc :any) {
+        doc.save();
+      }
+   });
+  }
+
+  ngOnInit(): void {
+      // this.saveToPDF()
+  }
 }
