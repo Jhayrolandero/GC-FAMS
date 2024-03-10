6742fe00464126aaab1256c554d11b4f10ec3814
viacrusis_chris - RD&P Committee <113983506+paranoid-android12@users.noreply.github.com>
2024-03-11 05:01:36 +0800

    Dynamic CV

--- src/app/components/faculty/faculty-profile/faculty-expertise/faculty-expertise.component.ts
+++ src/app/components/faculty/faculty-profile/faculty-expertise/faculty-expertise.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { mainPort } from '../../../../app.component';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Expertise } from '../../../../services/Interfaces/expertise';
 
 @Component({
   selector: 'app-faculty-expertise',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './faculty-expertise.component.html',
   styleUrl: './faculty-expertise.component.css'
 })
 export class FacultyExpertiseComponent {
+  @Input() expertise?: Expertise[];
+  @Output() setSpecEdit = new EventEmitter<Expertise>();
+  @Output() setType = new EventEmitter<string>();
 
+  //Send selected resume info on form component
+  sendValueParams(value: Expertise) {
+    this.setSpecEdit.emit(value);
+  }
+
+  //Change form type.
+  changeType(value: string) {
+    this.setType.emit(value);
+  }
+  
+  
+  showCrud = false;
+  tempPort = mainPort;
 }
