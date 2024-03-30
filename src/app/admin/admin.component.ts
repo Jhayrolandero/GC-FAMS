de1ccf19e96b11488787ec3e0134d1709771f5d7
viacrusis_chris - RD&P Committee <113983506+paranoid-android12@users.noreply.github.com>
2024-03-30 21:41:36 +0800

    Angular Materials based sidebar, also added more evaluation categories.

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -6,6 +6,9 @@ import { CommonModule } from '@angular/common';
 import { MessageComponent } from '../components/message/message.component';
 import { MessageService } from '../services/message.service';
 import { Message } from '../services/Interfaces/message';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
 @Component({
   selector: 'app-admin',
   standalone: true,
@@ -14,13 +17,17 @@ import { Message } from '../services/Interfaces/message';
     RouterOutlet,
     TopnavComponent,
     CommonModule,
-    MessageComponent
+    MessageComponent,
+    MatSidenavModule, 
+    MatButtonModule, 
+    FormsModule,
   ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
   sideBarToggle = true;
+  opened: boolean | undefined;
 
   toggle(){
     this.sideBarToggle = !this.sideBarToggle;
