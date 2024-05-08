410f417dbc53cca82ea1367840c535b10682ac3a
Cjay Landero <xjaylandero@gmail.com>
2024-05-08 22:57:28 +0800

    merge with main

--- src/app/services/crypto-js.service.ts
+++ src/app/services/crypto-js.service.ts
@@ -32,4 +32,22 @@ export class CryptoJSService {
 
   }
 
+  CryptoJSAesEncrypt(passphrase: string, plain_text: string) {
+
+    var salt = CryptoJS.lib.WordArray.random(256);
+    var iv = CryptoJS.lib.WordArray.random(16);
+
+    var key = CryptoJS.PBKDF2(passphrase, salt, { hasher: CryptoJS.algo.SHA512, keySize: 64 / 8, iterations: 999 });
+
+    var encrypted = CryptoJS.AES.encrypt(plain_text, key, { iv: iv });
+
+    var data = {
+      ciphertext: CryptoJS.enc.Base64.stringify(encrypted.ciphertext),
+      salt: CryptoJS.enc.Hex.stringify(salt),
+      iv: CryptoJS.enc.Hex.stringify(iv)
+    }
+
+    return JSON.stringify(data);
+  }
+
 }
